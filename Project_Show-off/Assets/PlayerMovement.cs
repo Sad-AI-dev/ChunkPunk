9eacd19cd509ade9d4055921d42bae0d64ba0fd1
jj <jjrosenberg122@gmail.com>
2022-04-28 14:12:18 +0200

    3rd person camera

---
+++ Project_Show-off/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody human;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        human = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.onActionTriggered += playerInput_onAction;
+    }
+
+
+    private void playerInput_onAction(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+    }
+
+    private void Movement(InputAction.CallbackContext context)
+    {
+        human.AddForce(0, 0, 2);
+    }
+}
