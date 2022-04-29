9c78ffc52da64eecedb9dc98a4e6a5cac011ff30
jj <jjrosenberg122@gmail.com>
2022-04-29 15:53:50 +0200

    CameraPhysics

--- Project_Show-off/Assets/Scripts/PlayerInputs/Player.cs
+++ Project_Show-off/Assets/Scripts/PlayerInputs/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float constantSpeed;
     Vector2 toMove;
     Rigidbody playerBody;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Vector3 m_EulerAngleVelocity;
 
     private void Start()
     {
@@ -22,7 +24,13 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         //transform.position += new Vector3(toMove.x, 0, toMove.y) * (moveSpeed * Time.deltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * toMove.x);
+        playerBody.AddForce(new Vector3(0, 0, toMove.y) * (moveSpeed * Time.deltaTime));
 
-        playerBody.AddForce(new Vector3(toMove.x, 0, toMove.y) * (moveSpeed * Time.deltaTime));
+        if(playerBody.velocity.magnitude < 2.0f)
+        {
+            //playerBody.AddForce(new Vector3(0, 0, constantSpeed));
+        }
+        playerBody.MoveRotation(playerBody.rotation * deltaRotation);
     }
 }
