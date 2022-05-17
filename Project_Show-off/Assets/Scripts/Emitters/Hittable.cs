e4812839109e4adea3c04e7acf23c26b7f8d20c2
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2022-05-17 14:28:45 +0200

    added costs + UI to hittables

--- Project_Show-off/Assets/Scripts/Emitters/Hittable.cs
+++ Project_Show-off/Assets/Scripts/Emitters/Hittable.cs
@@ -1,16 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class Hittable : MonoBehaviour
 {
-    [SerializeField] UnityEvent<Player> onHit;
+    [SerializeField] int price = 0;
+    [SerializeField] TMP_Text priceLabel;
+    [SerializeField] UnityEvent onHit;
+
+    Player buyer;
+    bool bought = false;
+
+    private void Start()
+    {
+        priceLabel.text = price.ToString();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.TryGetComponent(out Projectile proj)) {
-            onHit?.Invoke(proj.owner);
+            if (bought && proj.owner == buyer) { Activate(); }
+            else {
+                TryPurchase(proj.owner);
+            }
         }
+    }
+
+    void TryPurchase(Player p)
+    {
+        if (CoinManager.instance.TryBuy(p, price)) {
+            bought = true;
+            buyer = p;
+            Activate();
+        }
+    }
+
+    void Activate()
+    {
+        onHit?.Invoke();
     }
 }
