8778c2596207643fe35da509e64baae5e1a40c1c
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2022-05-18 12:11:40 +0200

    cleaned up inputreciever class

--- Project_Show-off/Assets/Scripts/PlayerInputs/InputReciever.cs
+++ Project_Show-off/Assets/Scripts/PlayerInputs/InputReciever.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem;
 public class InputReciever : MonoBehaviour
 {
     Player target;
-    Respawn respawn;
 
     private void Start()
     {
@@ -16,67 +15,27 @@ public class InputReciever : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        target?.SetMoveDir(context.ReadValue<Vector2>());
+        if (target) { target.SetMoveDir(context.ReadValue<Vector2>()); }
     }
-
-    public void Accelerate(InputAction.CallbackContext context)
-    {
-        Vector3 playerSpeed = context.ReadValue<Vector2>();
-        if (playerSpeed.y > 0.2)
-        {
-            target.Accelerating(true);
-
-        }
-        else
-        {
-            target.Accelerating(false);
-        }
-    }
-
-    public void Slow(InputAction.CallbackContext context)
-    {
-        Vector3 playerSpeed = context.ReadValue<Vector2>();
-        if(playerSpeed.y < -0.2)
-        {
-            target.Slowing(true);
-
-        } else
-        {
-            target.Slowing(false);
-        }
-    }
-
 
     public void Die(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {
-            target?.Died();
+        if (context.started) {
+            if (target) { target.Died(); }
         }
     }
     public void Looking(InputAction.CallbackContext context)
     {
-        Vector2 direction = context.ReadValue<Vector2>();
-        Debug.Log("at all");
-        if(direction.x < -0.2)
-        {
-            target?.Look(-1);
-            Debug.Log("Here");
-        } else if(direction.x > 0.2)
-        {
-            target?.Look(1);
-        }
-
-        if (context.canceled)
-        {
-            target?.Look(0);
+        if (target) {
+            if (context.canceled) { target.Look(Vector2.zero); }
+            else { target.Look(context.ReadValue<Vector2>()); }
         }
     }
     
     public void SouthButton(InputAction.CallbackContext context)
     {
         if (context.started) {
-            target?.Shoot();
+            if (target) { target.Shoot(); }
         }
     }
 
--- Project_Show-off/Assets/Scripts/PlayerInputs/PlayerManager.cs
+++ Project_Show-off/Assets/Scripts/PlayerInputs/PlayerManager.cs
@@ -20,18 +20,30 @@ public class PlayerManager : MonoBehaviour
     public readonly List<Player> players = new();
     int linkedPlayerCount = 0; //keeps track of how many of the players have been linked
 
+    [Header("Camera aim Settings")]
+    [SerializeField] List<GameObject> neutralVCams = new();
+    [SerializeField] List<GameObject> AimVCams = new();
+
     [Header("UI Settings")]
     public List<GameObject> playerUI = new();
 
     public void AddPlayer(Player player)
     {
         players.Add(player);
+        SetupPlayer(player);
         players.Sort((Player a, Player b) => a.id.CompareTo(b.id));
         if (GoalManager.instance != null) { //add player to external
             GoalManager.instance.players.Add(player.transform);
             GameplayManager.instance.scores.Add(player, 0);
             CoinManager.instance.money.Add(player, 0);
         }
+    }
+
+    void SetupPlayer(Player player)
+    {
+        int index = player.id - 1;
+        player.neutralVCam = neutralVCams[index];
+        player.aimVCam = AimVCams[index];
     }
 
     public Player GetUnlinkedPlayer()
