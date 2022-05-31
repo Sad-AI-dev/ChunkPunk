2f38906e9362b7826a7d73f143420ee3c1db8ebc
jj <jjrosenberg122@gmail.com>
2022-05-31 14:30:52 +0200

    Landmine that can be shot

--- Project_Show-off/Assets/Scripts/LandMine.cs
+++ Project_Show-off/Assets/Scripts/LandMine.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class LandMine : MonoBehaviour
 {
     [SerializeField] Vector3 force;
+    [SerializeField] float radius;
+    
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("collision");
         if (other.transform.TryGetComponent(out Player player))
         {
             Rigidbody body = player.GetComponent<Rigidbody>();
             body.AddForceAtPosition(force, this.transform.position);
+        }
+        else if (other.transform.TryGetComponent(out Projectile projectile))
+        {
+            Debug.Log("hit with bullet");
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.TryGetComponent(out Rigidbody body))
+                {
+                    body.AddForceAtPosition(force, this.transform.position);
+
+                }
+            }
         }
     }
 }
