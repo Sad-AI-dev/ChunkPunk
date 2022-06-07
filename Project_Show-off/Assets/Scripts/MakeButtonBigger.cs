e6b0fd6c85edd73733c65d695bdc11c9ad87fd67
jj <jjrosenberg122@gmail.com>
2022-06-07 10:43:11 +0200

    le buttons again

--- Project_Show-off/Assets/Scripts/MakeButtonBigger.cs
+++ Project_Show-off/Assets/Scripts/MakeButtonBigger.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class MakeButtonBigger : MonoBehaviour
 {
-    UnityEvent hoverPlay;
-
-    void Start()
+    RectTransform button;
+    [SerializeField] int HeightIncrease;
+    [SerializeField] int WidthIncrease;
+    private void Awake()
     {
-        if (hoverPlay == null)
-            hoverPlay = new UnityEvent();
-
-        hoverPlay.AddListener(bigBoy);
+        button = GetComponent<RectTransform>();
     }
-
-
-    private void bigBoy()
+    public void makeBigger()
     {
-
+        button.sizeDelta = new Vector2(button.rect.width + WidthIncrease, button.rect.height + HeightIncrease);
+    }
+    public void makeSmaller()
+    {
+        button.sizeDelta = new Vector2(button.rect.width - WidthIncrease, button.rect.height - HeightIncrease);
     }
 }
