e17a7b96efff7d5a596308a17a095b856b5c4b04
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2022-06-08 13:36:07 +0200

    fixed coin manager merge issue

--- Project_Show-off/Assets/Scripts/Coins/CoinManager.cs
+++ Project_Show-off/Assets/Scripts/Coins/CoinManager.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class CoinManager : MonoBehaviour
 {
-    [SerializeField] int maximumBullets;
+    public int maximumBullets;
     //--------singleton-------
     private void Awake()
     {
